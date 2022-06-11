b71f2d32e590eb776cbe36bd6674e31648cb7710
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-11 17:31:54 +0300

    commit tree b082694251720457451471c29b8ef75c210eef79

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const body = document.querySelector('body')
+body.removeChild(document.querySelector('#main'));
+
+const newHeader = document.createElement('h1');
+newHeader.id  = 'victory';
+newHeader.textContent = 'Sylvia Boyani is the champion';
+body.appendChild(newHeader); 
